730362e063e4d22bb396cb58420fd627c1bf1ea3
Camila Dias <mylinhaa.94@gmail.com>
2025-04-28 19:39:35 -0300

    feat: storage, ipc and react query for comunication and database

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,9 @@
 import { app, shell, BrowserWindow, ipcMain } from "electron";
 import path, { join } from "node:path";
-import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import { electronApp, optimizer } from "@electron-toolkit/utils";
 import icon from "../../resources/icon.png?asset";
+
+import "./ipc";
 
 // @ts-ignore
 import { registerRoute } from "../lib/electron-router-dom.js";
---
+++ src/main/ipc.ts
@@ -0,0 +1,3 @@
+import { ipcMain } from "electron";
+
+ipcMain.on("fetch-documents", (event, params) => {});
